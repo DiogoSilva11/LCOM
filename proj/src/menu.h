fddf710cbc5e54cfab23d634c0f67e23331ecbe7
JoAPSo Pedro Matos de AraAojo <up202007855@fe.up.pt>
2022-06-11 17:06:28 +0000

    menu.h done

---
+++ proj/src/menu.h
@@ -0,0 +1,18 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+
+#include <lcom/lcf.h>
+
+#include "timer_info.h"
+#include "kbd.h"
+#include "vbe_func.h"
+
+#include "info.h"
+#include "controller.h"
+#include "sprites.h"
+
+int (main_menu)();
+
+int (displayMenu)();
+
+#endif /* _MENU_H */
