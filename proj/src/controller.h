1a43bda988940068337945a01e4297dd0909f4cf
JoAPSo Pedro Matos de AraAojo <up202007855@fe.up.pt>
2022-06-11 17:07:01 +0000

    controller.h done

---
+++ proj/src/controller.h
@@ -0,0 +1,17 @@
+#ifndef _CONTROLLER_H_
+#define _CONTROLLER_H_
+
+#include <lcom/lcf.h>
+
+#include "timer_info.h"
+#include "kbd.h"
+#include "vbe_func.h"
+
+#include "info.h"
+#include "sprites.h"
+
+int (init)();
+
+int (leave)();
+
+#endif /* _CONTROLLER_H */
