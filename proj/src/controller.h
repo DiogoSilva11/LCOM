fe23ecedd1fc2aea82724c51cf4a210db701bd39
JoAPSo Pedro Matos de AraAojo <up202007855@fe.up.pt>
2022-06-13 09:48:19 +0000

    Replace controller.h

--- proj/src/controller.h
+++ proj/src/controller.h
@@ -6,12 +6,31 @@
 #include "timer_info.h"
 #include "kbd.h"
 #include "vbe_func.h"
+#include "mouse.h"
 
 #include "info.h"
 #include "sprites.h"
 
+/** @defgroup controller controller
+ * @{
+ * 
+ * Functions for initialization and termination of devices
+ */
+
+/**
+ * @brief Execute the initialization actions for all devices
+ * 
+ * @return Return 0 upon success and non-zero otherwise
+ */
 int (init)();
 
+/**
+ * @brief Execute the termination actions for all devices
+ * 
+ * @return Return 0 upon success and non-zero otherwise
+ */
 int (leave)();
+
+/**@}*/
 
 #endif /* _CONTROLLER_H */
